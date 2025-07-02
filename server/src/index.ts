1cb8399212f26b43c0130cef34c7e49bbd488dd0
Pratik Kad <kadpratik22@gmail.com>
2025-07-02 20:36:21 +0530

    websocket server for chat room!

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,36 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const wss= new WebSocketServer({port:8080});
 
+interface User{
+   socket:WebSocket,
+    roomId:string
+}
+
+const allsockets: User[] = [];
+
 wss.on("connection", (socket)=>{
     console.log("connection successful");
-    socket.on("message", (e)=>{
-        console.log(e.toString());
+
+    socket.on("message", (message)=>{
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if(parsedMessage.type == "join"){
+            console.log("User joined room # ", parsedMessage.payload.roomId);
+            
+            allsockets.push({
+                socket,
+                roomId:parsedMessage.payload.roomId
+            })
+        }
+
+        if(parsedMessage.type == "chat"){
+            const currentUserRoom = allsockets.find(x => x.socket == socket)?.roomId;
+            
+            allsockets.forEach(item =>{
+                if( item.roomId == currentUserRoom){
+                    item.socket.send(parsedMessage.payload.message);
+                }
+            })
+        }
     })
 })
