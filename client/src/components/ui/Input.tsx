94675f74dfec52cb169309763767018a1c88b6f8
Pratik Kad <kadpratik22@gmail.com>
2025-07-02 22:07:06 +0530

    UI components

---
+++ client/src/components/ui/Input.tsx
@@ -0,0 +1,26 @@
+interface InputInterface {
+    type: string,
+    placeholder: string,
+    name: string,
+    value?: string,
+    label?: string
+    onChange?: () => void
+    size: "sm" | "md" | "lg",
+    reference?:any
+}
+
+const sizeStyles = {
+    sm: "",
+    md: "",
+    lg: ""
+}
+
+const Input = (props: InputInterface) => {
+    return (
+        <input ref={props.reference}
+        className={`${sizeStyles[props.size]} w-full outline-none border border-custom-light-800 rounded-md px-4 py-2 text-zinc-100 `}
+        type={props.type} placeholder={props.placeholder} required value={props.value} name={props.name} onChange={props.onChange} />
+    )
+}
+
+export default Input
